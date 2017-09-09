a7a72cab56d2f1681d0d77263d3203b226a97740
Spencer Heywood <l.spencer.heywood@gmail.com>
2017-09-09 11:11:38 -0600

    linkfinder script added and requirements.txt updated with new dependencies

---
+++ linkfinder.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import httplib2
+import sys
+from bs4 import BeautifulSoup, SoupStrainer
+
+try:
+	website = sys.argv[1]
+	http = httplib2.Http()
+	status, response = http.request(website)
+
+	for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
+    		if link.has_attr('href'):
+        		print(link['href'])
+except IndexError:
+	print('syntax: ' + sys.argv[0] + ' http://google.com')
