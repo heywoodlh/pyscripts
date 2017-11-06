f8e4ead7b487c03aa65987cc3dba2a1fba28b2bc
Spencer Heywood <l.spencer.heywood@gmail.com>
2017-11-06 13:46:07 -0700

    fixed Beautiful Soup syntax

--- linkfinder.py
+++ linkfinder.py
@@ -8,7 +8,7 @@ try:
 	http = httplib2.Http()
 	status, response = http.request(website)
 
-	for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
+	for link in BeautifulSoup(response, "html.parser", parse_only=SoupStrainer('a')):
     		if link.has_attr('href'):
         		print(link['href'])
 except IndexError:
